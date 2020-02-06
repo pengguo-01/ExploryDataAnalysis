c5b3d61cebbd3598ec69abb44d9f69a4621ec7fa
pengguo-01 <58931032+pengguo-01@users.noreply.github.com>
2020-02-06 12:41:32 -0600

    plot1

---
+++ plot1.R
@@ -0,0 +1,27 @@
+library("data.table")
+setwd("C://Users//pg000//Desktop")
+Consum  <- data.table::fread(input = "household_power_consumption.txt", 
+                             na.strings ="?"
+)
+
+head(Consum)
+dim(Consum)
+?strptime
+## set the date format 
+
+subsetdata <- Consum[Consum$Date %in% c("1/2/2007","2/2/2007"),]
+## filter the data
+
+## convert string to numeric format 
+GlobActPoe <-as.numeric(subsetdata$Global_active_power)
+
+## make and save plot 
+## png graphic devices for BMP, JPEG, PNG
+
+png("plot1.png", width = 480, height = 480) 
+hist(GlobActPoe, col="red", main ="Gloabal Active Power",ylab= "Frequency")
+dev.off()
+
+
+
+
