b708e39e3d8b1f7788d75edb344849aba9c3a057
pengguo-01 <58931032+pengguo-01@users.noreply.github.com>
2020-02-06 12:43:38 -0600

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,15 @@
+## project 3
+sub1 <- as.numeric(subsetdata$Sub_metering_1)
+sub2 <- as.numeric(subsetdata$Sub_metering_2)
+sub3 <- as.numeric(subsetdata$Sub_metering_3)
+
+# save the plot 
+
+png("plot3.png", width = 480, height = 480)
+plot(datetime, sub1,type="l", col ="black", xlab ="", ylab="Engery sub metering")
+lines(datetime, sub2, col ="red")
+lines(datetime, sub3, col ="blue")
+
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty =c(1,1,1), col =c("black", "red", "blue"))
+dev.off()
